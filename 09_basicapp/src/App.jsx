13d3ddbd4b383745bce592bf64f70163b439dfa1
VamsiKrishna-web <alivelivamsikrishna@gmail.com>
2025-12-05 11:19:33 +0530

    Added images to app

--- 09_basicapp/src/App.jsx
+++ 09_basicapp/src/App.jsx
@@ -4,16 +4,70 @@ import { useState } from 'react'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
 
+  const pets = [
+    {
+      name: "Puppy",
+      type: "Dog",
+      image: "/dog_2.jpg",
+      skills: ["Barking", "Playing", "Eating"]
+
+    },
+    {
+      name: "Kitty",
+      type: "Cat",
+      image: "/cat_1.jpg",
+      skills: ["Meowing", "Climbing", "Sleeping"]
+    },
+
+    {
+      name: "Tammy",
+      type: "Dog",
+      image: "/dog_1.jpg",
+      skills: ["Digging", "Running", "Jumping"]
+
+    },
+
+    {
+      name: "Dimy",
+      type: "Cat",
+      image: "/cat_2.jpg",
+      skills: ["Purring", "Hunting", "Scratching"]
+    }
+  ]
   return (
-    <>
-      <div>
-        <h1> Hello world</h1>
-      </div>
+    //   This is JSXsyntax
+    <div>
+      <h2> My pets</h2>
+      <ul className="container">
+        {
+          pets.map((pet, index) => (
+            <li key={index}>
+              <div className="pet-header">
+                <h3>{pet.name}</h3>
+                <h4>Type {pet.type}</h4>
+              </div>
+              <div className="pet-body">
+                <img src={pet.image} alt={pet.name} width="200" height="200" />
 
-    </>
-  )
+                <ul className="skills">
+                  {
+                    pet.skills.map((skill, index) => (
+                      <li key={index}>{skill}</li>
+                    ))
+                  }
+                </ul>
+              </div>
+
+
+            </li>
+          ))
+        }
+      </ul>
+    </div>
+
+
+  );
 }
 
 export default App
